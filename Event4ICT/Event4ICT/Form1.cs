5912e63b66face5e4f1b9c3c17cb724935188954
diieuhanh <hanh-013@hotmail.com>
2015-10-28 15:27:37 +0100

    All commit

--- Event4ICT/Event4ICT/Form1.cs
+++ Event4ICT/Event4ICT/Form1.cs
@@ -7,14 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Oracle.DataAccess;
+using Oracle.DataAccess.Client;
 
 namespace Event4ICT
 {
     public partial class Form1 : Form
     {
+        Database database = new Database();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            database.Query = "SELECT * FROM antwoord ORDER BY antwoordid";
+            database.OpenConnection();
+
+
+            OracleDataReader reader = database.Command.ExecuteReader();
+
+
+            List<Account> account = new List<Account>();
+            while (reader.Read())
+            {
+                account.Add(new Account(Convert.ToInt32(reader["AccountID"]), Convert.ToString(reader["AccountType"]), Convert.ToString(reader["EmailAddress"]), Convert.ToString(reader["VisitorIDRFID"]), Convert.ToString(reader["Password"]), Convert.ToString(reader["FirstName"]), Convert.ToString(reader["Surename"]), Convert.ToString(reader["Address"]), Convert.ToString(reader["Country"]), Convert.ToString(reader["PhoneNumber"]), Convert.ToString(reader["BankAccount"])));
+            }
+
+            foreach (Account item in account)
+            {
+                label1.Text = item.FirstName;
+            }
+            Refresh();
+            database.CloseConnection();
         }
     }
 }
